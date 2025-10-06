6358f5e5834739f21f87129e8bf7da20e87076f0
Gustavo Rosa <gstvcaixeta@gmail.com>
2025-10-06 18:24:13 -0300

    retornando resposta de forma correta

--- server.js
+++ server.js
@@ -8,15 +8,16 @@ const ai = new GoogleGenAI({});
 
 const gerarPrompt = (angulos) => {
   return `
-  Gere um pre-diagnostico cefalometrico com base nestes angulos: ${JSON.stringify(angulos)}
+  Gere um pré-diagnóstico cefalométrico com base nestes ângulos: ${JSON.stringify(angulos)}
 
   Responda **somente em JSON** com o seguinte formato:
-{
-  "diagnostico": "Resumo do pre-diagnostico",
-  "recomendacoes: ["Sugestao 1", "Sugestao 2", "Sugestao 3", "Sugestao 4"]" -> Aqui no maximo 4
-}
-`;
-}
+  {
+    "diagnostico": "Resumo do pré-diagnóstico",
+    "recomendacoes": ["Sugestao 1", "Sugestao 2", "Sugestao 3", "Sugestao 4"]
+  }
+  `;
+};
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -24,23 +25,41 @@ app.use(cors());
 app.post("/diagnostico", async (req, res) => {
   try {
     const angulos = req.body;
-    console.log(angulos);
+    console.log("Ângulos recebidos:", JSON.stringify(angulos, null, 2));
 
-    // chamada para a IA
-    const response = await ai.models.generateContent({
+    const result = await ai.models.generateContent({
       model: "gemini-2.5-flash",
-      contents: gerarPrompt(angulos)
+      contents: gerarPrompt(angulos),
     });
 
-    //const data = await response.json();
 
-    console.log(response.text);
 
-    //const diagnostico = data?.output?.[0]?.content?.[0]?.text || "Não foi possível gerar diagnóstico";
+    // Extrai texto da resposta do modelo
+    const text = result.candidates?.[0]?.content?.parts?.[0]?.text;
 
-    //response.json({ diagnostico });
+    if (!text) {
+      throw new Error("A IA não retornou texto na resposta.");
+    }
+
+    const cleanText = text.replace(/```json|```/g, "").trim();
+
+    console.log("Resposta bruta da IA:", text);
+
+    // Faz parse do JSON retornado pela IA
+    let parsed;
+    try {
+      parsed = JSON.parse(cleanText);
+    } catch (err) {
+      console.error("Erro no parse.");
+    }
+
+    return res.json({
+      diagnostico: parsed.diagnostico,
+      recomendacoes: parsed.recomendacoes,
+    });
+
   } catch (err) {
-    console.error(err);
+    console.error("Erro ao gerar diagnóstico:", err);
     res.status(500).json({ error: "Erro ao gerar diagnóstico" });
   }
 });
